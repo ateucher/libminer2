ee35a76aae2b610118ce84e9f6ba97db2ca57684
Andy Teucher <andy.teucher@gmail.com>
2023-08-03 15:22:03 -0700

    Import purrr::map_dbl and use in lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,12 +3,16 @@
 #' Provides a brief summary of the package
 #' libraries on your machine
 #'
+#' @param sizes a logical indicating whether or not to calculate
+#'    sizes
+#'
 #' @return A data.frame containing the count
 #'    of packages in each of your libraries.
 #' @export
 #'
 #' @examples
 #' lib_summary()
+#' lib_summary(sizes = TRUE)
 lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
@@ -16,12 +20,9 @@ lib_summary <- function(sizes = FALSE) {
   names(pkg_df) <- c("library", "n_packages")
 
   if (sizes) {
-    pkg_df$lib_size <- vapply(
+    pkg_df$lib_size <- map_dbl(
       pkg_df$library,
-      function(x) {
-        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-      },
-      FUN.VALUE = double(1)
+        ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
     )
   }
   pkg_df
